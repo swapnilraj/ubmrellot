e722786c4a6d2164b93b51ccea03278bb892771a
Swapnil Raj <raj.swapnil99@gmail.com>
2020-02-18 23:59:19 +0000

    Get forcast for a particular time

--- weather.js
+++ weather.js
@@ -1,21 +1,46 @@
 var axios = require('axios');
 
 function URL(city, time) {
-  if (!city || !time) return { err: 'City or time missing' }
-  return 'https://api.openweathermap.org/data/2.5/weather?q=' + city +
-         'appid=' + process.env.WTHR_KEY;
+  if(!city) {
+    return { err: 'city missing' };
+  } else if (!time) {
+    return { url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city +
+            '&appid=' + process.env.WTHR_KEY + '&units=metric',
+            err: false
+           }
+  } else {
+    return { url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city +
+            '&appid=' + process.env.WTHR_KEY + '&units=metric',
+            err: false
+           }
+  }
+
 }
 
-function formatForecast(resp) {
-  return resp.weather.description;
+function formatForecast(resp, time) {
+  if(!time) {
+    return resp.weather[0].description;
+  }
+
+  var DIFFERENCE_OF_3HOURS = 10800100
+    , timeInEpoch = new Date(time).getTime();
+
+  for (var i = 0; i < resp.cnt; ++i) {
+    var dayWeather = resp.list[i];
+
+    if (Math.abs(dayWeather - timeInEpoch) < DIFFERENCE_OF_3HOURS) {
+      return JSON.stringify(dayWeather);
+    }
+  }
 }
 
 async function forecast(city, time) {
-  var url = URL(city, time);
-  if (url.err) return 'Sorry could not find forecast for your query';
+  var res = URL(city, time);
+  if (res.err) return 'Sorry could not find forecast for your query because ' + url.err;
 
-  var response = await axios(url);
-  var msg = formatForecast(response);
+  var response = await axios(res.url);
+      response = await response.data;
+  var msg = formatForecast(response, time);
   return msg + 'You asked for ' + time + ' but I didn\'t implement that lol';
 }
 
